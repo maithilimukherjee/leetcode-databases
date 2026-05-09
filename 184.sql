ae259cdb05ee643fdf1a221f423c47faa39c0126
maithili <86041684+maithilimukherjee@users.noreply.github.com>
2026-05-09 23:24:01 +0530

    Add SQL query to find highest salary by department

---
+++ 184.sql
@@ -0,0 +1,9 @@
+select d.name as department, e.name as employee, e.salary as salary
+from employee e
+join department d
+on e.departmentid = d.id
+where (e.departmentid, e.salary) in (
+    select departmentid, max(salary)
+    from employee
+    group by departmentid
+);
